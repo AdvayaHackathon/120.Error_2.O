c831fec88ed0a46e051ef0e5da8f65384ab7978d
golu91 <golu7903057@gmail.com>
2025-04-12 06:33:13 +0530

    Components

---
+++ APP/config/credential.ts
@@ -0,0 +1,9 @@
+export const GOOGLE_CLOUD_CREDENTIALS = {
+    api_key: "AIzaSyDjXKdxCfd5ChvXBQIyLiMEh83EbMU30N0", // Replace with your actual Gemini API key
+  } as const;
+  
+  export const API_KEYS = {
+    GOOGLE_MAPS: 'AIzaSyDEtagQ9SfI5yTjkM7Ir-g9hk54Vy2ut_k',
+    WEATHER: '59776ceea11651e624c095d8eb8625f3'
+  } as const;
+  
